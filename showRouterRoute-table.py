e9b553ad967335dd0e3cf95b7c3ccef55ed6af3a
Baris Ozensel <94804863+bozensel@users.noreply.github.com>
2022-02-13 16:36:04 +0300

    Update showRouterRoute-table.py

--- showRouterRoute-table.py
+++ showRouterRoute-table.py
@@ -23,3 +23,58 @@ print(results)
 
 #converting str to json. 
 result = json.loads(results)
+
+'''
+RESULT:
+[
+    {
+        "RouteTable": [
+            {
+                "Age": "15d22h01m",
+                "Dest_Prefix_Flags_": "10.10.10.1/32",
+                "Metric": "0",
+                "Next_Hop_Interface_Name": "system",
+                "Pref": "0",
+                "Proto": "Local",
+                "Type": "Local"
+            },
+            {
+                "Age": "17h12m34s",
+                "Dest_Prefix_Flags_": "10.10.10.3/32",
+                "Metric": "50",
+                "Next_Hop_Interface_Name": "192.168.10.2",
+                "Pref": "10",
+                "Proto": "OSPF",
+                "Type": "Remote"
+            },
+            {
+                "Age": "17h11m56s",
+                "Dest_Prefix_Flags_": "10.10.10.121/32",
+                "Metric": "100",
+                "Next_Hop_Interface_Name": "192.168.10.2",
+                "Pref": "10",
+                "Proto": "OSPF",
+                "Type": "Remote"
+            },
+            {
+                "Age": "18h20m52s",
+                "Dest_Prefix_Flags_": "192.168.10.0/27",
+                "Metric": "0",
+                "Next_Hop_Interface_Name": "toSR7-3",
+                "Pref": "0",
+                "Proto": "Local",
+                "Type": "Local"
+            },
+            {
+                "Age": "17h12m34s",
+                "Dest_Prefix_Flags_": "192.168.20.0/27",
+                "Metric": "100",
+                "Next_Hop_Interface_Name": "192.168.10.2",
+                "Pref": "10",
+                "Proto": "OSPF",
+                "Type": "Remote"
+            }
+        ]
+    }
+]
+'''
