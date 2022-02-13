9dc14f2eab35280ba9a89be738c2590b8ea5071d
Baris Ozensel <94804863+bozensel@users.noreply.github.com>
2022-02-13 16:38:38 +0300

    Update showUsers.py

--- showUsers.py
+++ showUsers.py
@@ -20,4 +20,39 @@ parser.parse()
 results = parser.result(format='json')[0]
 print(results)
 
-
+'''
+[
+    {
+        "showUsers": [
+            {
+                "From": "--",
+                "Session_ID": "6"
+            },
+            {
+                "Idle_day": "0d",
+                "Idle_time": "00:00:00",
+                "Login_Date": "08FEB2022",
+                "Login_Time": "10:53:29",
+                "Type": "SSHv2",
+                "User": "admin                           "
+            },
+            {
+                "From": "135.244.199.185",
+                "Session_ID": "132"
+            },
+            {
+                "Idle_day": "0d",
+                "Idle_time": "00:03:35",
+                "Login_Date": "09FEB2022",
+                "Login_Time": "11:32:50",
+                "Type": "SSHv2",
+                "User": "admin                           "
+            },
+            {
+                "From": "10.144.208.82",
+                "Session_ID": "143"
+            }
+        ]
+    }
+]
+'''
