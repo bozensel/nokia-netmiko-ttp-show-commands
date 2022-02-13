85e40b8793dc1a15547f9343a129251347e1548b
Baris Ozensel <94804863+bozensel@users.noreply.github.com>
2022-02-13 16:36:54 +0300

    Update showRouterTunnel-table.py

--- showRouterTunnel-table.py
+++ showRouterTunnel-table.py
@@ -18,3 +18,30 @@ parser.parse()
 # print result in JSON format
 results = parser.result(format='json')[0]
 print(results)
+
+'''
+[
+    {
+        "Show_Router_Ospf_Neighbor": [
+            {
+                "Area_ID": "0.0.0.0",
+                "Interface-Name": "toSR7-3",
+                "Neighbor_State": "Full",
+                "Pri": "1",
+                "RetxQ": "0",
+                "Rtr_Id": "10.10.10.3",
+                "TTL": "33"
+            },
+            {
+                "Area_ID": "1.1.1.1",
+                "Interface-Name": "toSR7-4",
+                "Neighbor_State": "Full",
+                "Pri": "10",
+                "RetxQ": "0",
+                "Rtr_Id": "10.10.12.3",
+                "TTL": "34"
+            }
+        ]
+    }
+]
+'''
