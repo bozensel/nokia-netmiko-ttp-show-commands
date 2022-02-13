85986de265845dbb20759926db56cd5bb027fc21
Baris Ozensel <94804863+bozensel@users.noreply.github.com>
2022-02-13 16:37:32 +0300

    Update showServiceService-using.py

--- showServiceService-using.py
+++ showServiceService-using.py
@@ -18,3 +18,52 @@ parser.parse()
 # print result in JSON format
 results = parser.result(format='json')[0]
 print(results)
+
+'''
+[
+    {
+        "Show_Service_Service-using": [
+            {
+                "Admin_State": "Down",
+                "Customer_id": "1",
+                "Oper_State": "Down",
+                "ServiceId": "10",
+                "Service_Name": "10",
+                "Service_TYPE": "VPRN"
+            },
+            {
+                "Admin_State": "Down",
+                "Customer_id": "1",
+                "Oper_State": "Down",
+                "ServiceId": "56",
+                "Service_Name": "56",
+                "Service_TYPE": "Mirror"
+            },
+            {
+                "Admin_State": "Up",
+                "Customer_id": "1",
+                "Oper_State": "Up",
+                "ServiceId": "500",
+                "Service_Name": "500",
+                "Service_TYPE": "VPRN"
+            },
+            {
+                "Admin_State": "Up",
+                "Customer_id": "1",
+                "Oper_State": "Down",
+                "ServiceId": "2147483648",
+                "Service_Name": "_tmnx_InternalIesService",
+                "Service_TYPE": "IES"
+            },
+            {
+                "Admin_State": "Up",
+                "Customer_id": "1",
+                "Oper_State": "Down",
+                "ServiceId": "2147483649",
+                "Service_Name": "_tmnx_InternalVplsService",
+                "Service_TYPE": "intVpls"
+            }
+        ]
+    }
+]
+'''
